73108ba5abc054511d2848865675e3ec46f1aef6
MerlinEgalite <egalite.merlin@gmail.com>
2023-11-07 09:46:14 +0300

    refactor: renaming + fix tests

--- test/mocks/ERC20PermissionedMock.sol
+++ test/mocks/ERC20PermissionedMock.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC20GatedBase, ERC20, IERC20} from "../../src/ERC20GatedBase.sol";
+import {ERC20PermissionedBase, ERC20, IERC20} from "../../src/ERC20PermissionedBase.sol";
 
-contract ERC20GatedMock is ERC20GatedBase {
+contract ERC20PermissionedMock is ERC20PermissionedBase {
     mapping(address => bool) internal _hasPermission;
 
     constructor(string memory name_, string memory symbol_, IERC20 underlyingToken, address morpho, address bundler)
-        ERC20GatedBase(name_, symbol_, underlyingToken, morpho, bundler)
+        ERC20PermissionedBase(name_, symbol_, underlyingToken, morpho, bundler)
     {}
 
     function hasPermission(address account) public view override returns (bool) {
